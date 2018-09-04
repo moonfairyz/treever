e93c3e55a6850c3bb36f03051c62434a275de978
Raditha Dissanayake <raditha@raditha.com>
2018-09-04 14:57:37 +0530

    Added a simple test case. Edited some templates to make it slightly easier to test

--- order/tests.py
+++ order/tests.py
@@ -1,3 +1,42 @@
-from django.test import TestCase
+import time
+from django.contrib.staticfiles.testing import StaticLiveServerTestCase
+from selenium import webdriver
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
 
-# Create your tests here.
+from django.urls import reverse
+
+class MySeleniumTests(StaticLiveServerTestCase):
+    fixtures = ['user.json', 'products.json']
+    def __init__(self, *args, **kwargs):
+       
+        super().__init__(*args, **kwargs)
+        
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
+        cls.selenium = webdriver.Chrome()
+        cls.selenium.implicitly_wait(10)
+
+    @classmethod
+    def tearDownClass(cls):
+        cls.selenium.quit()
+        super().tearDownClass()
+
+    def get_url(self, relative_path):
+        self.selenium.get('%s%s' % (self.live_server_url, relative_path))
+        
+    def test_login(self):
+        self.get_url('/account/login/')
+        username_input = self.selenium.find_element_by_id("id_username")
+        username_input.send_keys('user')
+        password_input = self.selenium.find_element_by_id("id_password")
+        password_input.send_keys('user 123')
+        
+        self.selenium.find_element_by_css_selector(".login").click()
+        WebDriverWait(self.selenium, 5).until(EC.presence_of_element_located((By.TAG_NAME, 'body')))
+        self.get_url('/shop/allproducts/apple-tree/')
+        WebDriverWait(self.selenium, 5).until(EC.presence_of_element_located((By.TAG_NAME, 'body')))
+        self.selenium.find_element_by_css_selector(".cart-add").click()
+        time.sleep(10);
--- order/views.py
+++ order/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render, get_object_or_404
+from django.urls import reverse
+
 from .models import Order
 
 def thanks(request, order_id):
